c7825b94783bd2de1ace6e731f4bcf40a62c07d4
Rishi Advani <rishi1999@gmail.com>
2018-11-03 22:59:12 -0400

    added project to git repo

---
+++ click.js
@@ -0,0 +1,66 @@
+var canvas = document.createElement('canvas');
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+canvas.style.position = "absolute";
+document.body.appendChild(canvas);
+var ctx = canvas.getContext("2d");
+canvas.addEventListener('click', handleClick);
+
+function handleClick() {
+    let mouseX;
+    let mouseY;
+    const bounds = canvas.getBoundingClientRect();
+
+    mouseX = event.pageX - bounds.left - scrollX;
+    mouseY = event.pageY - bounds.top - scrollY;
+
+    mouseX *= canvas.width / bounds.width; 
+    mouseY *= canvas.height / bounds.height;
+
+    createClickDesign(mouseX, mouseY);
+}
+
+var degree = 3, density = 10, initialBarLength = 1, barIncrement = 0.02, radius = 1000, patternLength = 1;
+
+function createClickDesign(mouseX, mouseY) {
+    for (let i = 0; i < density; i++) {
+        for (let j = 0; j < degree; j++) {
+            generate(mouseX, mouseY, mouseX + Math.cos(i*2*Math.PI/degree) * initialBarLength, mouseY + Math.sin(i*2*Math.PI/degree) * initialBarLength, 0, Math.floor(Math.random() * 2 + 1), Math.floor(radius / initialBarLength), initialBarLength);
+        }
+    }
+    
+}
+
+function generate(x0, y0, x1, y1, patternCount, lastDir, count, barLength) {
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    const colorVal = 255 - count/radius*initialBarLength*255;
+    ctx.strokeStyle="rgb(" + colorVal + ", " + colorVal + ", " + colorVal + ")";
+    ctx.stroke();
+
+    if (count <= 0) {
+        return;
+    }
+
+    let dir;
+
+    if (patternCount < patternLength) {
+        dir = lastDir;
+    } else {
+        dir = Math.floor(Math.random() * 2) + 1;
+        patternCount = 0;
+    }
+
+    let angle = Math.atan2(y1 - y0, x1 - x0);
+    const angleDelta = Math.random() * Math.PI;
+    if (dir == 1) {
+        angle += angleDelta;
+    } else {
+        angle -= angleDelta;
+    }
+    const x2 = x1 + (barLength + barIncrement) * Math.cos(angle);
+    const y2 = y1 + (barLength + barIncrement) * Math.sin(angle);
+
+    generate(x1, y1, x2, y2, patternCount + 1, dir, count - 1, barLength + barIncrement);
+}
